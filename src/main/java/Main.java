7ee2e51c876bf4d8161296fe3a8dbed149b39a38
Serhii <serg14581@gmail.com>
2019-05-16 20:58:15 +0300

    test request

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,11 @@ import com.mashape.unirest.http.Unirest;
 public class Main {
     public static final String ApiKey="AIzaSyAINCiahYJ9DOkX5heKnPYtJyoVm8oqdPc";
     public static void main(String[] args) throws Exception{
-        String url="https://www.googleapis.com/youtube/v3/search";
+        String url="https://www.googleapis.com/youtube/v3/channels";
         HttpResponse<String> response = Unirest.get(url)
                 .queryString("key", ApiKey)
-                .queryString("part", "snippet")
-                .queryString("maxResults",1)
-                .queryString("q","UCf31Gf5nCU8J6eUlr7QSU0w")
+                .queryString("part", "snippet, statistics")
+                .queryString("id","UCf31Gf5nCU8J6eUlr7QSU0w")
                 .asString();
         System.out.println(response.getBody());
     }
