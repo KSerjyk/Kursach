952cdea3e9a471051cd5f608acbafa03036daea9
Serhii <serg14581@gmail.com>
2019-05-16 20:24:20 +0300

    channelId

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args){
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
 
+public class Main {
+    public static final String ApiKey="AIzaSyAINCiahYJ9DOkX5heKnPYtJyoVm8oqdPc";
+    public static void main(String[] args) throws Exception{
+        String url="https://www.googleapis.com/youtube/v3/search";
+        HttpResponse<String> response = Unirest.get(url)
+                .queryString("key", ApiKey)
+                .queryString("part", "snippet")
+                .queryString("maxResults",1)
+                .queryString("q","UCf31Gf5nCU8J6eUlr7QSU0w")
+                .asString();
+        System.out.println(response.getBody());
     }
 }
---
+++ src/main/java/YoutubeChannel/ChannelId.java
@@ -0,0 +1,14 @@
+package YoutubeChannel;
+
+public class ChannelId {
+    private String kind;
+    private String channelId;
+
+    public String getKind() {
+        return kind;
+    }
+
+    public String getChannelId() {
+        return channelId;
+    }
+}
