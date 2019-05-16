fa826b61d793ca5de406780451a2669fac277084
Serhii <serg14581@gmail.com>
2019-05-16 20:05:53 +0300

    channelStats

---
+++ src/main/java/YoutubeChannel/ChannelStatistic.java
@@ -0,0 +1,29 @@
+package YoutubeChannel;
+
+public class ChannelStatistic {
+    private Long viewCount;
+    private Long commentCount;
+    private Long subscriberCount;
+    private boolean hiddenSubscriberCount;
+    private Long videoCount;
+
+    public Long getViewCount() {
+        return viewCount;
+    }
+
+    public Long getCommentCount() {
+        return commentCount;
+    }
+
+    public Long getSubscriberCount() {
+        return subscriberCount;
+    }
+
+    public boolean isHiddenSubscriberCount() {
+        return hiddenSubscriberCount;
+    }
+
+    public Long getVideoCount() {
+        return videoCount;
+    }
+}
